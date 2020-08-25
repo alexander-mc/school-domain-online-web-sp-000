5fb4fcca2b660186c56894902e80952b463ec2ca
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-25 04:11:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
   end
 
   def add_student(name, grade)
-    If @roster[grade] == nil
+    If self.@roster[grade] == nil
       puts "correct"
     #   @roster[grade] = []
     #   @roster[grade] << name
